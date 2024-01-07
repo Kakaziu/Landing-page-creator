5a969cafcebc09ce0ab9da668804ad8f548e1d29
Kauã <kaua.borba03@gmail.com>
2024-01-07 13:57:47 -0300

    test: text component

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -1,8 +1,18 @@
+import { screen } from '@testing-library/react';
 import TextComponent from '.';
 import renderTheme from '../../styles/render-theme';
 
 describe('<TextComponent />', () => {
-  it('should render Text component', () => {
-    renderTheme(<TextComponent>oi</TextComponent>);
+  it('should render with correct children', () => {
+    renderTheme(<TextComponent>alou</TextComponent>);
+    const text = screen.getByText('alou');
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>alou</TextComponent>);
+
+    expect(container).toMatchSnapshot();
   });
 });
