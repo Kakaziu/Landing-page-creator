71b5184ed902d45ed2f5b9cbc3c9cfdf08b1c62d
Kauã <kaua.borba03@gmail.com>
2024-01-06 23:51:30 -0300

    create: heading component

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,13 +1,26 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-const Heading = ({ children, isdark = true }) => {
-  return <Styled.Title isdark={isdark}>{children}</Styled.Title>;
+const Heading = ({
+  children,
+  isdark = true,
+  as = 'h1',
+  uppercase = false,
+  size = 'small',
+}) => {
+  return (
+    <Styled.Title isdark={isdark} as={as} uppercase={uppercase} size={size}>
+      {children}
+    </Styled.Title>
+  );
 };
 
 Heading.propTypes = {
   children: P.node,
   isdark: P.bool,
+  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  uppercase: P.bool,
+  size: P.oneOf(['small', 'medium', 'big', 'huge']),
 };
 
 export default Heading;
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,7 +1,34 @@
 import styled, { css } from 'styled-components';
 
+const titleSizes = {
+  small: (theme) => css`
+    font-size: ${theme.fonts.sizes.medium};
+  `,
+
+  medium: (theme) => css`
+    font-size: ${theme.fonts.sizes.large};
+  `,
+
+  big: (theme) => css`
+    font-size: ${theme.fonts.sizes.xxlarge};
+  `,
+
+  huge: (theme) => css`
+    font-size: ${theme.fonts.sizes.xhuge};
+    ${mediaFont(theme)}
+  `,
+};
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.fonts.sizes.xxlarge};
+  }
+`;
+
 export const Title = styled.h1`
-  ${({ theme, isdark }) => css`
-    color: ${isdark ? theme.colors.primaryColor : theme.colors.white}
+  ${({ theme, isdark, uppercase, size }) => css`
+    color: ${isdark ? theme.colors.primaryColor : theme.colors.white};
+    text-transform: ${uppercase ? 'uppercase' : 'none'};
+    ${titleSizes[size](theme)}
   `}
 `;
