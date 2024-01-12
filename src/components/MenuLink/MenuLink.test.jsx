eae20a1735d4966b3f2b344ca01269a14c4df105
Kauã <kaua.borba03@gmail.com>
2024-01-11 23:08:01 -0300

    test: MenuLink component

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -1,8 +1,27 @@
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import MenuLink from '.';
+import renderTheme from '../../styles/render-theme';
 
 describe('<MenuLink/>', () => {
-  it('should you write something here', () => {
-    render(<MenuLink />);
+  it('should render link', () => {
+    const { debug } = renderTheme(
+      <MenuLink link="http://localhost">oi</MenuLink>,
+    );
+    const link = screen.getByRole('link', { name: 'oi' });
+
+    expect(link).toHaveAttribute('target', '_self');
+    debug();
+  });
+
+  it('should render link open in new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost:3000" open_in_new_tab={false}>
+        oi
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'oi' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
   });
 });
--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -1,16 +1,16 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-const MenuLink = ({ text, link, open_in_new_tab = false }) => {
+const MenuLink = ({ children, link, open_in_new_tab = true }) => {
   return (
     <Styled.Link href={link} target={open_in_new_tab ? '_self' : '_blank'}>
-      {text}
+      {children}
     </Styled.Link>
   );
 };
 
 MenuLink.propTypes = {
-  text: P.string.isRequired,
+  children: P.string.isRequired,
   link: P.string.isRequired,
   open_in_new_tab: P.bool,
 };
