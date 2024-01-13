0c24b222ad35eeb7f5aaeaf78969045b15803bc5
Kauã <kaua.borba03@gmail.com>
2024-01-12 22:53:55 -0300

    test: NavLink component

--- src/components/NavLink/NavLink.test.jsx
+++ src/components/NavLink/NavLink.test.jsx
@@ -1,8 +1,24 @@
-import { render } from '@testing-library/react';
+import renderTheme from '../../styles/render-theme';
 import NavLink from '.';
+import links from './mock';
+import { screen } from '@testing-library/react';
 
 describe('<NavLink/>', () => {
-  it('should you write something here', () => {
-    render(<NavLink />);
+  it('should render all links', () => {
+    renderTheme(<NavLink links={links} />);
+
+    expect(screen.getAllByRole('link')).toHaveLength(links.length);
+  });
+
+  it('should not render links', () => {
+    renderTheme(<NavLink />);
+
+    expect(screen.queryAllByRole('link')).toHaveLength(0);
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<NavLink links={links} />);
+
+    expect(container).toMatchSnapshot();
   });
 });
