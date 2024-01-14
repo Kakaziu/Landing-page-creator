4825a305c9bc13ce98ba742bbd7eaa800b2b7549
Kauã <kaua.borba03@gmail.com>
2024-01-14 14:49:35 -0300

    test: SectionTwoColumns

--- src/components/SectionTwoColumns/SectionTwoColumns.test.jsx
+++ src/components/SectionTwoColumns/SectionTwoColumns.test.jsx
@@ -1,8 +1,20 @@
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import SectionTwoColumns from '.';
+import renderTheme from '../../styles/render-theme';
+import mock from './mock';
 
 describe('<SectionTwoColumns/>', () => {
-  it('should you write something here', () => {
-    render(<SectionTwoColumns />);
+  it('should render gridTwoColumns', () => {
+    const { container } = renderTheme(<SectionTwoColumns {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should title render with white color when background is activated', () => {
+    renderTheme(<SectionTwoColumns {...mock} background />);
+    expect(
+      screen.getByRole('heading', { name: 'Grid two columns' }),
+    ).toHaveStyle({
+      color: 'rgb(255, 255, 255)',
+    });
   });
 });
