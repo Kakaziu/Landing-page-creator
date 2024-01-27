b868e503aec55aa4e218c42f7b0002d5c20dc8fd
Kauã <kaua.borba03@gmail.com>
2024-01-26 23:37:40 -0300

    fix: test grid-image component

--- src/components/GridImage/GridImage.test.jsx
+++ src/components/GridImage/GridImage.test.jsx
@@ -1,17 +1,29 @@
+import { screen } from '@testing-library/react';
 import GridImage from '.';
 import renderTheme from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 import mock from './mock';
 
 describe('<GridImage/>', () => {
   it('should render with background', () => {
-    const { container } = renderTheme(<GridImage {...mock} />);
+    const { container } = renderTheme(<GridImage {...mock} background />);
     expect(container).toMatchSnapshot();
+    expect(container.firstChild).toHaveStyle({
+      background: theme.colors.primaryColor,
+    });
   });
 
   it('should render without background', () => {
-    const { container } = renderTheme(
-      <GridImage {...mock} background={undefined} />,
-    );
-    expect(container).toMatchSnapshot();
+    const { container } = renderTheme(<GridImage {...mock} />);
+
+    expect(container.firstChild).toHaveStyle({
+      background: theme.colors.white,
+    });
+  });
+
+  it('should not render grid when no have grid', () => {
+    renderTheme(<GridImage />);
+
+    expect(screen.queryAllByAltText(/teste/i)).toHaveLength(0);
   });
 });
